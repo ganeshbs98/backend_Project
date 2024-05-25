ba80ffc6e397e31c5ca7d8139e3e2bc920c4da86
Balamurugan <balamurugan.p@foodhub.com>
2024-05-25 16:10:09 +0530

    removed unwanted codes

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -25,8 +25,3 @@ export const uploadOnCloundinary=async(loaclFilePath)=>{
     }
 
 }
-
-
-// cloudinary.uploader.upload("https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg",
-//   { public_id: "olympic_flag" }, 
-//   function(error, result) {console.log(result); });
